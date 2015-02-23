33a5283afccffccb9de5a293874b206614596179
Eirik <eiriksh@stud.ntnu.no>
2015-02-23 12:42:32 +0100

    Oppdatert Login med java.docs

--- src/no/ntnu/fellesprosjekt34/Login.java
+++ src/no/ntnu/fellesprosjekt34/Login.java
@@ -1,5 +1,57 @@
 package no.ntnu.fellesprosjekt34;
 
 public class Login {
+	
+
+	private String email;
+	private String password;
+	
+	/**
+	 * The Constructor takes two arguments, email and password of the user. 
+	 * @param email
+	 * @param password
+	 */
+	public Login(String email, String password) {
+		this.email = email;
+		this.password = password;
+	}
+	
+	
+	/**
+	 * 
+	 * @return The email of the user. 
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * Sets the email for the object. 
+	 * @param email 
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * 
+	 * @return The users password.
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	
+	/**
+	 * Sets the password for the object. 
+	 * @param password
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	
+	
+	
 
 }
