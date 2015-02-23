a151b52b9ecb33abc39e3dbe45427a54be87a6f5
Eirik <eiriksh@stud.ntnu.no>
2015-02-23 11:40:19 +0100

    Legger til Appoinment-klassen i Appointment-branchen

---
+++ src/no/ntnu/fellesprosjekt34/Appointment.java
@@ -0,0 +1,38 @@
+package no.ntnu.fellesprosjekt34;
+
+import java.time.LocalTime;
+import java.util.Date;
+
+
+public class Appointment {
+	
+	//Variabler
+	private String description;
+	private String shortName;
+	private LocalTime startTime;
+	private LocalTime endTime;
+	private Date startDate;
+	private Date endDate;
+	
+	public Appointment(String description, String shortName, LocalTime startTime, LocalTime endTime, Date startDate, Date endDate){
+		
+		
+	}
+	
+	
+	//Getter, setter and validation for description
+	public String getDescription(){
+		return description;
+	}
+	
+	public void setDescription(String description){
+		
+	}
+	
+	
+	
+	
+	
+	
+
+}
