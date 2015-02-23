4f930d5b2b709657eb2b4a32d772e0323d937169
Eirik <eiriksh@stud.ntnu.no>
2015-02-23 13:48:24 +0100

    Laget tester for email og passord.

--- src/no/ntnu/fellesprosjekt34/Login.java
+++ src/no/ntnu/fellesprosjekt34/Login.java
@@ -1,8 +1,9 @@
 package no.ntnu.fellesprosjekt34;
 
-public class Login {
-	
 
+public class Login {	
+
+	//Variables
 	private String email;
 	private String password;
 	
@@ -12,8 +13,15 @@ public class Login {
 	 * @param password
 	 */
 	public Login(String email, String password) {
-		this.email = email;
-		this.password = password;
+		
+		//Må få inn try/catch
+		if(validateEmail(email)){
+			this.email = email;			
+		}
+		
+		if(validatePassword(password)){
+			this.password = password;			
+		}
 	}
 	
 	
@@ -51,24 +59,79 @@ public class Login {
 	 * Sets the password for the object. 
 	 * @param password
 	 */
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	
-	private boolean validateEmail(String email){
+	public boolean setPassword(String password) {
 		
-		String[] splitMail = email.split("@");
-		
-		System.out.println("Dette funker ikke");
-		
+		if(validatePassword(password)){
+			this.password = password;
+			return true;
+		}
 		
 		return false;
 	}
 	
-	public static void Main(String[] args){
-		Login test = new Login("eirik@hamnvik.no", "123123");
-		test.setEmail("eirik@hamnvik.no");
+	/**
+	 * Validates if email is allowed. Needs to be one '@'. 
+	 * @param email
+	 * @return boolean, true if done correctly, false otherwise. 
+	 */
+	private boolean validateEmail(String email){
+		
+		//Splits at '@'
+		String[] splitMail = email.split("@");
+		
+		//Can only be one '@'.
+		if(splitMail.length != 2){
+			System.out.println("Needs one '@'");
+			return false;
+		}
+		
+		//Before the '@', it is only allowed with letters, numbers and '.', '_' and '-'.
+		if(!splitMail[0].matches("[A-Za-z0-9\\.\\_\\-]+")){
+			System.out.println("Some characters are not allowed.");
+			return false;
+		}
+		
+		//If it is good, return true;
+		return true;
+	}
+	
+	/**
+	 * Validates the password. Needs to be at least 8 characters long and contain at least
+	 * one upper case letter and one digit. 
+	 * @param password
+	 * @return
+	 */
+	private boolean validatePassword(String password){
+		
+		//Needs to be at least 8 characters long. 
+		if(password.length() < 8){
+			System.out.println("Password must be at least 8 characters");
+			return false;
+		}
+		
+		//Needs to be at least one upper case letter and one digit
+		boolean upper = false;
+		boolean digit = false;
+		for (char c : password.toCharArray()) {
+			if(Character.isUpperCase(c)){
+				upper = true;
+			}
+			
+			else if(Character.isDigit(c)){
+				digit = true;
+			}
+		}
+		
+		if(!upper || !digit){
+			System.out.println("Needs to be at least one upper case letter and a number.");
+			return false;
+		}
+		
+		return true;
+	}
+	
+	public static void main(String[] args){
+		Login test = new Login("eirik-@hamnvik.no", "123123");
 	}
 
 	
