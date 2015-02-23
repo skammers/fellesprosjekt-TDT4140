6a794added7f585a97cb1fe5da7a67eecf26ee24
Ludvig Jordet <ludvig.jordet@gmail.com>
2015-02-23 11:12:58 +0100

    Add group-table

--- DB.sql
+++ DB.sql
@@ -1,9 +1,23 @@
 CREATE TABLE User(
-    id INT NOT NULL PRIMARY KEY
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY
     email VARCHAR(50),
     firstName VARCHAR(30),
     lastName VARCHAR(30),
     position VARCHAR(30),
     phoneNumber VARCHAR(20),
     hashedPassword CHAR(64),
+)
+
+CREATE TABLE Group(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30),
+    ownerId INT NOT NULL,
+    FOREIGN KEY (ownerId) REFERENCES User.id
+)
+
+CREATE TABLE GroupUser(
+    userId INT NOT NULL,
+    groupId INT NOT NULL,
+    FOREIGN KEY (userId) REFERENCES User.id,
+    FOREIGN KEY (groupId) REFERENCES Group.id
 )
