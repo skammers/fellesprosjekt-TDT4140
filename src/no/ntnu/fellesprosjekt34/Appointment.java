9fcf57566817e6a8fc438ec7bc3146c606e75a4a
Eirik <eiriksh@stud.ntnu.no>
2015-02-23 12:05:56 +0100

    Lagt til gettere og settere i appointment, i tillegg til å sjekke tiden.

--- src/no/ntnu/fellesprosjekt34/Appointment.java
+++ src/no/ntnu/fellesprosjekt34/Appointment.java
@@ -1,33 +1,91 @@
 package no.ntnu.fellesprosjekt34;
 
-import java.time.LocalTime;
-import java.util.Date;
+import java.time.LocalDateTime;
 
 
 public class Appointment {
-	
+
 	//Variabler
 	private String description;
 	private String shortName;
-	private LocalTime startTime;
-	private LocalTime endTime;
-	private Date startDate;
-	private Date endDate;
+	private LocalDateTime startTime;
+	private LocalDateTime endTime;
 	
-	public Appointment(String description, String shortName, LocalTime startTime, LocalTime endTime, Date startDate, Date endDate){
+	public Appointment(String description, String shortName, LocalDateTime startTime, LocalDateTime endTime){
 		
+		this.description = description;
+		this.shortName = shortName;
+		
+		if(checkTime(startTime, endTime)){
+			this.startTime = startTime;
+			this.endTime = endTime;
+		}
 		
 	}
 	
 	
-	//Getter, setter and validation for description
+	//Getter and setter for description
 	public String getDescription(){
 		return description;
 	}
 	
 	public void setDescription(String description){
+		this.description = description;
+	}
+	
+	public String getShortName() {
+		return shortName;
+	}
+
+
+	public void setShortName(String shortName) {
+		this.shortName = shortName;
+	}
+
+
+	public LocalDateTime getStartTime() {
+		return startTime;
+	}
+
+
+	public boolean setStartTime(LocalDateTime startTime) {
+		
+		if(checkTime(startTime, this.endTime)){
+			this.startTime = startTime;
+			return true;
+		}
+		return false;
 		
 	}
+
+
+	public LocalDateTime getEndTime() {
+		return endTime;
+	}
+
+
+	public boolean setEndTime(LocalDateTime endTime) {
+		
+		if(checkTime(this.startTime, endTime)){
+			this.endTime = endTime;
+			return true;
+		}
+		return false;
+	}
+	
+
+	//Validating start- and endTime
+	private boolean checkTime(LocalDateTime startTime, LocalDateTime endTime){
+		
+		if(startTime.isBefore(endTime)){
+			return true;
+		}
+		
+		return false;
+	}
+	
+	
+	
 	
 	
 	
