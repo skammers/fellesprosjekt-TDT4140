c2731718a10d5d2637dc594dd43b941f4a11b14e
xecor <xecor2k@hotmail.com>
2015-02-23 10:39:39 +0100

    set up user class

--- src/User.java
+++ src/User.java
@@ -1,4 +1,54 @@
 
 public class User {
 
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int phoneNumber;
+    private String position;
+
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+
+
 }
