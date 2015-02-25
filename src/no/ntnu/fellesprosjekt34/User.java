2bb9b841beee17777e52d5df8b0f1a47b7ddb9e9
mikail <mikael94@hotmail.no>
2015-02-25 14:23:11 +0100

    Fikset paa noen klasser og groupview
    jepp det gjorde jeg

--- src/no/ntnu/fellesprosjekt34/User.java
+++ src/no/ntnu/fellesprosjekt34/User.java
@@ -1,55 +1,74 @@
 package no.ntnu.fellesprosjekt34;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.StringProperty;
+
 public class User {
 
 
-    private String firstName;
-    private String lastName;
-    private String email;
-    private int phoneNumber;
-    private String position;
-
-
-    public int getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(int phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
+    private StringProperty firstName;
+    private StringProperty lastName;
+    private StringProperty email;
+    private IntegerProperty phoneNumber;
+    private StringProperty position;
 
     public String getFirstName() {
+        return firstName.get();
+    }
+
+    public StringProperty firstNameProperty() {
         return firstName;
     }
 
     public void setFirstName(String firstName) {
-        this.firstName = firstName;
+        this.firstName.set(firstName);
     }
 
     public String getLastName() {
+        return lastName.get();
+    }
+
+    public StringProperty lastNameProperty() {
         return lastName;
     }
 
     public void setLastName(String lastName) {
-        this.lastName = lastName;
+        this.lastName.set(lastName);
     }
 
     public String getEmail() {
+        return email.get();
+    }
+
+    public StringProperty emailProperty() {
         return email;
     }
 
     public void setEmail(String email) {
-        this.email = email;
+        this.email.set(email);
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber.get();
+    }
+
+    public IntegerProperty phoneNumberProperty() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber.set(phoneNumber);
     }
 
     public String getPosition() {
+        return position.get();
+    }
+
+    public StringProperty positionProperty() {
         return position;
     }
 
     public void setPosition(String position) {
-        this.position = position;
+        this.position.set(position);
     }
-
-
-
 }
