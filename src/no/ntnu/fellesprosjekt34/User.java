025b55c58edc104fe088d641036243b16f44a2c9
Ludvig Jordet <ludvig.jordet@gmail.com>
2015-02-26 15:36:46 +0100

    Add user-loading

--- src/no/ntnu/fellesprosjekt34/User.java
+++ src/no/ntnu/fellesprosjekt34/User.java
@@ -1,55 +1,56 @@
 package no.ntnu.fellesprosjekt34;
 
+import no.ntnu.fellesprosjekt34.server.Database;
+import no.ntnu.fellesprosjekt34.server.NotFoundInDBException;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class User {
 
+    private static final String QUERY = "SELECT * FROM User WHERE email = \"%s\" LIMIT 1";
 
     private String firstName;
     private String lastName;
     private String email;
-    private int phoneNumber;
+    private String phoneNumber;
     private String position;
 
 
-    public int getPhoneNumber() {
-        return phoneNumber;
+    public User(String firstName, String lastName, String email, String phoneNumber, String position) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.position = position;
     }
 
-    public void setPhoneNumber(int phoneNumber) {
-        this.phoneNumber = phoneNumber;
+    public static User getFromDB(Database db, String email) throws SQLException, NotFoundInDBException{
+        ResultSet r = db.Q(String.format(QUERY, email));
+        if(r.next()){
+            return new User(r.getString("firstName"), r.getString("lastName"), r.getString("email"), r.getString("phoneNumber"), r.getString("position"));
+        } else {
+            throw new NotFoundInDBException();
+        }
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
     }
 
     public String getFirstName() {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
     public String getLastName() {
         return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getPosition() {
         return position;
     }
-
-    public void setPosition(String position) {
-        this.position = position;
-    }
-
-
-
 }
