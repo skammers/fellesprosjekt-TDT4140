33295ec4b790fcd5a8456863842bee8649e112a1
Ludvig Jordet <ludvig.jordet@gmail.com>
2015-02-26 15:36:46 +0100

    Add user-loading

--- src/no/ntnu/fellesprosjekt34/server/CalendarServer.java
+++ src/no/ntnu/fellesprosjekt34/server/CalendarServer.java
@@ -1,6 +1,7 @@
 package no.ntnu.fellesprosjekt34.server;
 
 import no.ntnu.fellesprosjekt34.Room;
+import no.ntnu.fellesprosjekt34.User;
 
 import java.sql.SQLException;
 
@@ -9,9 +10,9 @@ public class CalendarServer {
     public static void main(String[] args) {
         Database db = new Database("jdbc:mysql://bull.ludvigjordet.com/kalender", "kalender", "unagi");
         try {
-            Room strossa = Room.getFromDB(db, "Samfundet", "Strossa");
-            System.out.println("Capacity: " + strossa.getCapacity());
-            System.out.println("Location: " + strossa.getLocation());
+            User strossa = User.getFromDB(db, "ludvig.jordet@gmail.com");
+            System.out.println("Capacity: " + strossa.getFirstName());
+            System.out.println("Location: " + strossa.getLastName());
         } catch (SQLException e) {
             e.printStackTrace();
         } catch (NotFoundInDBException e) {
