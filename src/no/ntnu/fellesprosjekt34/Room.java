87cce831a7402e2e28b9cbb59e02bb64a3c382b1
Ludvig Jordet <ludvig.jordet@gmail.com>
2015-02-26 15:19:05 +0100

    Forgot files. Fml.

--- src/no/ntnu/fellesprosjekt34/Room.java
+++ src/no/ntnu/fellesprosjekt34/Room.java
@@ -1,20 +1,39 @@
 package no.ntnu.fellesprosjekt34;
 
+import no.ntnu.fellesprosjekt34.server.Database;
+import no.ntnu.fellesprosjekt34.server.NotFoundInDBException;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 public class Room {
 	
 	private String location;
 	private String name;
 	private String building;
+    private int capacity;
+    private static final String QUERY = "SELECT * FROM Room WHERE roomName = \"%s\" AND buildingName = \"%s\" LIMIT 1";
 	
 	/**
 	 * Constructor for Room, takes three arguments: the location, name and building of the room
 	 */
-	public Room (String location, String name, String building) {
+	public Room (String location, String name, String building, int capacity) {
 		this.location = location;
 		this.name = name;
 		this.building = building;
+        this.capacity = capacity;
 	}
-	
+
+    public static Room getFromDB(Database db, String building, String name) throws SQLException, NotFoundInDBException{
+        ResultSet r = db.Q(String.format(QUERY, name, building));
+        if(r.next()){
+            return new Room(r.getString("location"), r.getString("roomName"), r.getString("buildingName"), r.getInt("capacity"));
+        } else {
+            throw new NotFoundInDBException();
+        }
+    }
+
 	/**
 	 * Method returns the location of the Room-object
 	 */
@@ -38,4 +57,10 @@ public class Room {
 	public String getBuilding() {
 		return building;
 	}
+
+    /**
+     * Returns the capacity of the room in number of people
+     * @return An int representing the number of people the room takes
+     */
+    public int getCapacity() { return capacity; }
 }
--- src/no/ntnu/fellesprosjekt34/server/Database.java
+++ src/no/ntnu/fellesprosjekt34/server/Database.java
@@ -1,49 +1,36 @@
 package no.ntnu.fellesprosjekt34.server;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Parameter;
 import java.sql.*;
 
 public class Database {
 
     Connection conn = null;
+    private String url, username, password;
 
-    public Database(){
+    public Database(String url, String username, String password){
+        this.url = url;
+        this.username = username;
+        this.password = password;
         try{
             Class.forName("com.mysql.jdbc.Driver").newInstance();
         } catch (Exception e){
             System.out.println("Kunne ikke loade DB-driver");
         }
-        try {
-            conn = DriverManager.getConnection("jdbc:mysql://bull.ludvigjordet.com/kalender", "kalender", "unagi");
-            /*Statement s = conn.createStatement();
-            ResultSet rs = s.executeQuery("SELECT * FROM User");
-            while(rs.next()){
-                System.out.println("Navn: " + rs.getString("firstName") + " " + rs.getString("lastName"));
-            }*/
-        } catch (SQLException e){
-            System.out.println("SQL-feil");
-            System.out.println(e.getMessage());
-        }
     }
     
-    private Connection getConnection(){
-        try {
-            if (conn != null && !conn.isClosed()){
-                return conn;
-            } else {
-                conn = DriverManager.getConnection("jdbc:mysql://bull.ludvigjordet.com/kalender", "kalender", "unagi");
-            }
-        } catch (SQLException e) {
-            conn = null;
+    private Connection getConnection() throws SQLException{
+        if (conn != null && !conn.isClosed()){
+            return conn;
+        } else {
+            conn = DriverManager.getConnection(url, username, password);
         }
         return conn;
     }
 
-    public ResultSet Q(String query){
-        try{
-            Statement s = getConnection().createStatement();
-            return s.executeQuery(query);
-        } catch (SQLException e) {
-            return null;
-        }
+    public ResultSet Q(String query) throws SQLException {
+        Statement s = getConnection().createStatement();
+        return s.executeQuery(query);
     }
 }
