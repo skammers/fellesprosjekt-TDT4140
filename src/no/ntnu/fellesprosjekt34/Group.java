2055b37bf6522615395620961f96f83492415adc
mikail <mikael94@hotmail.no>
2015-02-27 09:30:30 +0100

    Fikset group-klassen
    Lagde konstruktør og  get/set for variablene i Group

--- src/no/ntnu/fellesprosjekt34/Group.java
+++ src/no/ntnu/fellesprosjekt34/Group.java
@@ -1,10 +1,13 @@
 package no.ntnu.fellesprosjekt34;
 
+import javafx.beans.InvalidationListener;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 
-import java.util.ArrayList;
-import java.util.Observable;
+import java.util.*;
 
 /**
  * Created by mikail on 25/02/15.
@@ -16,10 +19,13 @@ public class Group {
     private StringProperty name;
     private ObservableList<User> users;
     //private Calendar groupCalendar;
-
+    public Group(String name){
+        this.name = new SimpleStringProperty();
+        this.users = FXCollections.observableArrayList();
+    }
 
     public String getName() {
-        return name.get();
+        return name.getValue();
     }
 
     public StringProperty nameProperty() {
@@ -28,7 +34,7 @@ public class Group {
 
     public void setName(String name) {
         if(name.length() >0){
-            this.name.set(name);
+            this.name.setValue(name);
         }
         else {
             System.out.println("Feil! Skriv inn et navn");
